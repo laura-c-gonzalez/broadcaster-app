1625c92d1b56c89178672349cd44ba02c6aeed70
Laura Gonzalez <lcgonzalez24@gmail.com>
2022-02-02 22:05:42 -0700

    previous/next buttons for scrolling through 'col' items in home component

---
+++ src/app/shared-services/multi-slide/prev.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+
+@Directive({
+  selector:'[appPrev]'
+})
+
+export class PrevDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('click')
+  prevFunc() {
+    let elm = this.el.nativeElement.parentElement.parentElement.children[0];
+    //looking for slider-wrap > slider-main and want to slide on items only thus children and first position
+  
+    let item = elm.getElementsByClassName("item");
+
+    elm.prepend(item[item.length - 1]);
+    //to get the last item
+  }
+}
--- src/app/shared-services/shared-service.module.ts
+++ src/app/shared-services/shared-service.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NextDirective } from './multi-slide/next.directive';
+import { PrevDirective } from './multi-slide/prev.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NextDirective,
+    PrevDirective],
   imports: [
     HttpClientModule
   ],
-  exports:[]
+  exports: [
+    NextDirective,
+    PrevDirective
+  ]
 })
 export class SharedServiceModule {}
 
