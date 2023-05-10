3481e26b86333d740d16e246464239bc700f654b
Laura Gonzalez <lcgonzalez24@gmail.com>
2023-05-09 21:07:00 -0600

    table with audio songs for my-playlist

--- src/app/my-playlist/my-playlist.component.ts
+++ src/app/my-playlist/my-playlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BackOfficeService } from '../shared-services/backoffice.service';
+import { Observable, debounceTime, distinctUntilChanged, BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-my-playlist',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPlaylistComponent implements OnInit {
 
-  constructor() { }
+  songs$!: Observable<any>;
+
+  private subjectKeyUp = new BehaviorSubject<any>([]);
+
+  //private subjectKeyUp = new Subject<any>();
+
+  constructor(private backofficeService: BackOfficeService) { }
 
   ngOnInit(): void {
+    this.subjectKeyUp.pipe(debounceTime(1000),
+      distinctUntilChanged())
+      .subscribe(d => {
+        this.loadCourses(d);
+      })
+  }
+
+  onSearch($event: any) {
+
+    const value = $event.target.value;
+    //this.subjectKeyUp.next(value);
+    console.log(value);
+    this.loadCourses(value);
+  }
+
+  loadCourses(value: Observable<any>) {
+
+
+
+    console.log("result of load courses " + this.songs$)
+
+//this.songs$ = this.backofficeService.getSongsById(id);
+
+this.songs$ = this.backofficeService.loadAllCourses(value);
+
+
+ 
   }
 
 }
--- src/app/search-music/music-detail/music-detail.component.ts
+++ src/app/search-music/music-detail/music-detail.component.ts
@@ -1,12 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackOfficeService } from '../../shared-services/backoffice.service';
-import { Iartists } from '../../home/Dto search/artists';
-import { Isongs } from '../../home/Dto search/songs';
-import { Observable, map, switchMap, fromEvent, debounceTime, distinctUntilChanged, noop, concat, of, Subject, BehaviorSubject } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthStore } from '../../shared-services/auth-store/auth-store';
-import { createHttpObservable } from '../../shared-services/utils';
-import { HttpClient } from '@angular/common/http';
+import { Observable, debounceTime, distinctUntilChanged, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-music-detail',
@@ -27,7 +21,7 @@ export class MusicDetailComponent implements OnInit {
       distinctUntilChanged())
       .subscribe(d => {
         this.loadCourses(d);
-    })
+      })
   }
 
   onSearch($event: any) {
@@ -39,89 +33,18 @@ export class MusicDetailComponent implements OnInit {
   }
 
   loadCourses(value: Observable<any>) {
-    console.log("result of load courses " +this.songs$)
+    console.log("result of load courses " + this.songs$)
 
     //this.songs$ = this.backofficeService.getSongsById(id);
 
     this.songs$ = this.backofficeService.loadAllCourses(value);
 
-  //  this.backofficeService.loadAllCourses(value).subscribe(
-  //    d => { console.log(d)
-  //    },
-  //    error => {
-  //    console.error(error);
-  //  })
+    //  this.backofficeService.loadAllCourses(value).subscribe(
+    //    d => { console.log(d)
+    //    },
+    //    error => {
+    //    console.error(error);
+    //  })
   }
-
-
-  //artists$!: Observable<Iartists[]>;
-
-  //songData;
-
-  //http$!: Observable<Isongs>;
-
-  //songs$!: Observable<Isongs[]>;
-
-  //isVisible: boolean = false;
-
-  //@ViewChild('searchInput')
-  //searchInput!: ElementRef;
-
-  //constructor(public authStore: AuthStore,
-  //  public router: Router,
-  //  private route: ActivatedRoute,
-  //  private backofficeService: BackOfficeService,
-  //  private http: HttpClient
-  //) { }
-
-  //ngOnInit() {
-
-
-  //  this.songData = this.route.snapshot.data["musicObj"];
-
-
-  //  this.songs$ = createHttpObservable(`/api/payload / ${this.songData}`);
-
-  //  console.log("this.songs$"+ this.songs$)
-
-
-  //  //http$.subscribe(
-  //  //  search => console.log(search),
-  //  //  noop,
-  //  //  () => console.log('completed')
-  //  //);
-  //}
-
-  //logout() {
-  //  this.authStore.logout();
-  //  this.router.navigate(['/broadcaster/login']);
-  //}
-
-  //ngAfterViewInit() {
-   
-  //  const searchArtists$ = fromEvent<any>(this.searchInput.nativeElement, 'keyup')
-  //    .pipe(
-  //      map(event => event.target.value),
-  //      debounceTime(400),
-  //      distinctUntilChanged(),
-  //      switchMap(search => this.loadArtists(search))
-  //    );
-
-  //  const initialArtists$ = this.loadArtists();
-
-  //  this.songs$ = concat(initialArtists$, searchArtists$);
-
-  //}
-
-  //loadArtists(search = ''): Observable<Isongs[]>{
-
-
-  //  let result = createHttpObservable(`/api/payload${this.songData}&filter=${search}`);
-
-  //  return createHttpObservable(`/api/payload?musicObj=${this.songData}&filter=${search}`)
-  //    //.pipe(
-  //    //  map((res: Isongs)=> res["payload"])
-  //    //)
-  //}
 
 }
